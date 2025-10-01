1e511958797a66359a79d3a4507a9d8562dfce2b
Allex <1allex.moura@gmail.com>
2025-10-01 08:21:18 -0300

    Event - repository, service, controller, validation

--- src/main/java/com/devsuperior/bds04/controllers/exceptions/InvalidTokenException.java
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/InvalidTokenException.java
@@ -3,7 +3,7 @@ package com.devsuperior.bds04.controllers.exceptions;
 public class InvalidTokenException extends RuntimeException{
 
     public InvalidTokenException(String msg) {
-        super(msg);
+        super("Invalid token");
     }
 
 }
--- src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -1,6 +1,9 @@
 package com.devsuperior.bds04.dto;
 
 import com.devsuperior.bds04.entities.Event;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -9,9 +12,15 @@ public class EventDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+
+	@NotBlank(message = "Campo requerido")
 	private String name;
+
+	@FutureOrPresent(message = "A data do evento não pode ser passada")
 	private LocalDate date;
 	private String url;
+
+	@NotNull(message = "Campo requerido")
 	private Long cityId;
 	
 	public EventDTO() {
