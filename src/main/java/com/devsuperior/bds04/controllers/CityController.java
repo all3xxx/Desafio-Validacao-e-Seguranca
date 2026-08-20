9dfe2741501b26dbcc75d42078e1cd59f8434f6d
Allex <1allex.moura@gmail.com>
2026-08-20 09:46:56 -0300

    City - repository, service, controller, validation. Testes aprovados

---
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -0,0 +1,36 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.services.CityService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> result = service.findAllSortedByName();
+        return ResponseEntity.ok(result);
+    }
+
+}
--- src/main/java/com/devsuperior/bds04/controllers/handler/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/bds04/controllers/handler/ControllerExceptionHandler.java
@@ -3,9 +3,12 @@ package com.devsuperior.bds04.controllers.handler;
 import com.devsuperior.bds04.controllers.exceptions.ClientWithoutPermissionException;
 import com.devsuperior.bds04.controllers.exceptions.InvalidTokenException;
 import com.devsuperior.bds04.dto.CustomErrorDTO;
+import com.devsuperior.bds04.dto.ValidationErrorDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,6 +28,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomErrorDTO> clientWithoutPermission(ClientWithoutPermissionException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.FORBIDDEN;
         CustomErrorDTO error = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorDTO> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+        ValidationErrorDTO error = new ValidationErrorDTO(Instant.now(), status.value(), "Validation error", request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(error);
     }
 
