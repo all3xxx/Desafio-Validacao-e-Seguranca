0c46b399ce36ea1be9653569a57129e590b8ac4e
Allex <1allex.moura@gmail.com>
2026-08-20 09:57:46 -0300

    Event - repository, service, controller, validation. Testes aprovados

---
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_CLIENT', 'ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> result = service.findAllPaged(pageable);
+        return ResponseEntity.ok(result);
+    }
+
+}
